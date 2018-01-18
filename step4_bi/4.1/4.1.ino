4a589f661fafbbc2b95583c21a6946febd5410ab
のびすけ <rsksound@gmail.com>
2018-01-18 11:55:11 +0900

    update

--- step4_bi/4.1/4.1.ino
+++ step4_bi/4.1/4.1.ino
@@ -1,30 +1,17 @@
-#include <Nefry.h>
-#include <NefryCloud.h>
+#include <NefryAmbient.h>
 
-NefryCloud nefryCloud;
-
-void onpush(String message);
+NefryAmbient nefryAmbient;
 
 void setup() {
-  nefryCloud.begin("n0bisuke","bb43e25cec77eb9d20127eb25612223ccd37bcaf647fc24973a646d1abe69f8e");//サイトで登録したuser,メールで受け取ったapikeyを入力してください
-  nefryCloud.on(onpush);
-  Nefry.setProgramName("NefryBT Nefry Cloud");
-  analogRead(A0);
+  nefryAmbient.begin();//ambientを使えるように初期設定をします。
+  Nefry.setProgramName("AmbientSimpleAnalog");//プログラム名を指定します。
 }
 
 void loop() {
-  nefryCloud.loop();
-}
-
-void onpush(String message) {//Nefryクラウド(仮)から通知が来ます
-  Nefry.print("onpush : ");
-  Nefry.println(message);
-
-  if(message.equals("red")){
-    Nefry.setLed(255,0,0);
-  }else if(message.equals("green")){
-    Nefry.setLed(0,255,0);
-  }else if(message.equals("blue")){
-    Nefry.setLed(0,0,255);
-  }
+  int analog;
+  analog = analogRead(A1);// A1ピンの状態を取得します。
+  Nefry.print("Analog:");Nefry.println(analog);//アナログの値を表示します。
+  nefryAmbient.set(1, analog); //field,dataの順にセットします。
+  nefryAmbient.send();//ambientにデータを送信します。
+  Nefry.ndelay(1000);//1秒待つ
 }
